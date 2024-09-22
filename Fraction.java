76cf8aa826700c0c0bff04731b84b6149f89b82c
uvsq22407865 <taousmeziane3@gmail.com>
2024-09-22 11:25:42 +0200

    ajouter tostring

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,10 @@
 public class Fraction {
     // vide pour le moment
 
+    @Override
+    public String toString() {
+        return "je suis une fraction.";
+
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        Fraction fraction = new Fraction();
+        System.out.println(fraction);
         System.out.println("le projet compile et s'execute");
     }
 
