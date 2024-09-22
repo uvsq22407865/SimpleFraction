d17dd2f9138cf886ffcccb24b6bd9ec9450dd6c7
uvsq22407865 <taousmeziane3@gmail.com>
2024-09-22 12:42:04 +0200

    constructeurs et assertions

--- Fraction.java
+++ Fraction.java
@@ -3,6 +3,25 @@ public class Fraction {
     private int numerateur;
     private int denominateur;
 
+    // Constructeur avec numerateur et denominateur
+
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    // Constructeur avec juste le numerateur
+    public Fraction(int numerateur) {
+        this.numerateur = numerateur;
+        this.denominateur = 1; // Denominateur par defaut
+    }
+
+    // Constructeur sans argument
+    public Fraction() {
+        this.numerateur = 0; // Numerateur par defaut
+        this.denominateur = 1; // Denominateur par defaut
+    }
+
     @Override
     public String toString() {
         return "je suis une fraction.";
--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Fraction fraction = new Fraction();
-        System.out.println(fraction);
+        Fraction fraction1 = new Fraction(8, 16);
+        assert fraction1.toString().equals("8/16") : "Test echoue pour fraction1";
 
-        System.out.println("le projet compile et s'execute");
+        Fraction fraction2 = new Fraction(7);
+        assert fraction2.toString().equals("7/1") : "Test echoue pour fraction1";
+
+        Fraction fraction3 = new Fraction();
+        assert fraction3.toString().equals("0/1") : "Test echoue pour fraction1";
+
+        System.out.println("Tous les tests sont passes !");
     }
 
 }
